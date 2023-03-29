1d3bc0c5ce0d6fa34ee344a6de49038f2444c27a
Maxim <toper.one111@gmail.com>
2023-03-29 22:55:07 +0300

    Initial setup

---
+++ main.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const log = require('electron-log');
+
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV === 'development';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'APP NAME',
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: './assets/icons/icon.png',
+    resizable: isDev,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile('./app/index.html');
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
