dee11a7d322724954a37e354194e8fda27a6a2c5
Maxim <toper.one111@gmail.com>
2023-04-02 22:39:07 +0300

    Provide some dynamic data to the interface
    Added CPU usage and free info
    Displayed and uptime in a user friendly manner

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,21 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+setInterval(() => {
+  // CPU usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerText = info.toFixed(2) + '%';
+  });
+
+  // CPU free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = info.toFixed(2) + '%';
+  });
+
+  // Uptime
+  document.getElementById('sys-uptime').innerText = secondsToDmhs(os.uptime());
+}, 2000);
+
 // Set model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -17,3 +32,13 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById('mem-total').innerText = info.totalMemMb;
 });
+
+// Show days, hours, minutes
+function secondsToDmhs(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
